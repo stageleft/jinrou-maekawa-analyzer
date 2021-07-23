6f55efc1cfa3e68259184b1eb5ba1d62086c1037
stageleft <sugano-github@elder-alliance.org>
2021-07-24 01:10:49 +0900

    Now on Research and  Development

--- create_xpi.ps1
+++ create_xpi.ps1
@@ -1,12 +1,12 @@
-# Powershell script to create jinrou-wakamete-analyzer.xpi
+# Powershell script to create jinrou-maekawa-analyzer.xpi
 
-if (Test-Path 'jinrou-wakamete-analyzer.xpi' ){
-    Remove-Item 'jinrou-wakamete-analyzer.xpi'
+if (Test-Path 'jinrou-maekawa-analyzer.xpi' ){
+    Remove-Item 'jinrou-maekawa-analyzer.xpi'
 }
-if (Test-Path 'jinrou-wakamete-analyzer.zip' ){
-    Remove-Item  'jinrou-wakamete-analyzer.zip'
+if (Test-Path 'jinrou-maekawa-analyzer.zip' ){
+    Remove-Item  'jinrou-maekawa-analyzer.zip'
 }
 
-Compress-Archive -Path 'src\*' -DestinationPath 'jinrou-wakamete-analyzer.zip'
+Compress-Archive -Path 'src\*' -DestinationPath 'jinrou-maekawa-analyzer.zip'
 
-Rename-Item 'jinrou-wakamete-analyzer.zip' 'jinrou-wakamete-analyzer.xpi'
+Rename-Item 'jinrou-maekawa-analyzer.zip' 'jinrou-maekawa-analyzer.xpi'
